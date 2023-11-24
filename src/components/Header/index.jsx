d34f6d8f53bbde8961a21960795f174c38f1d71e
Thiago Martins Bezerra <thiagomartinsb272@gmail.com>
2023-11-24 04:58:39 -0300

    Responsividade do Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,10 @@
 'use client'
 
+import { useState } from 'react'
+
 import styles from './styles.module.scss'
 
+import { FiMenu, FiX } from 'react-icons/fi'
 import { Button } from '@/components'
 
 import { useRouter } from 'next/navigation'
@@ -14,6 +17,10 @@ export default function Header() {
   const router = useRouter()
 
   const { isUserLogged, handleLogout, userType } = useAuth()
+
+  const [menuMobileIsOpen, setMenuMobileIsOpen] = useState(false)
+
+  const toggleMenuMobile = () => setMenuMobileIsOpen(!menuMobileIsOpen)
 
   return (
     <header className={styles.header}>
@@ -55,6 +62,52 @@ export default function Header() {
         ) : (
           <Button label="Entrar / Cadastrar" onClick={() => router.push('/')} />
         )}
+      </div>
+
+      <div className={styles.header_mobile}>
+        <div
+          className={styles.header_mobile__toggle}
+          onClick={toggleMenuMobile}
+        >
+          {menuMobileIsOpen ? <FiX /> : <FiMenu />}
+        </div>
+        <div
+          className={`${styles.header_mobile__menu} ${
+            menuMobileIsOpen && styles.opened
+          }`}
+        >
+          <div className={styles.header_auth__mobile}>
+            {isUserLogged &&
+              (userType === 'client' ? (
+                <Button label="Marcar consulta" onClick={() => {}} />
+              ) : (
+                <Button label="Criar consulta" onClick={() => {}} />
+              ))}
+
+            {isUserLogged ? (
+              <Button label="Sair" onClick={handleLogout} />
+            ) : (
+              <Button
+                label="Entrar / Cadastrar"
+                onClick={() => router.push('/')}
+              />
+            )}
+          </div>
+
+          <nav className={styles.header_navigation__mobile}>
+            <ul>
+              <li>
+                <Link href="/home">Home</Link>
+              </li>
+              <li>
+                <Link href="/calculadora">Calculadora de IMC</Link>
+              </li>
+              <li>
+                <Link href="/guia">Guia de treinos</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </header>
   )
